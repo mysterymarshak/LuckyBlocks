cbdcb9ebfcf5e1edec5674ea2f42ef3d4b9b046b
mysterymarshak <test1132100948@gmail.com>
2025-07-10 18:14:01 +0500

    reorder interface members

--- src/LuckyBlocks/Loot/WeaponPowerups/IWeaponPowerup.cs
+++ src/LuckyBlocks/Loot/WeaponPowerups/IWeaponPowerup.cs
@@ -7,8 +7,8 @@ internal interface IWeaponPowerup<out T> where T : Weapon
 {
     string Name { get; }
     T Weapon { get; }
-    void Run();
     IWeaponPowerup<T> Clone(Weapon copiedWeapon);
+    void Run();
     bool IsCompatibleWith(Type otherPowerupType);
     void MoveToWeapon(Weapon otherWeapon);
     void Dispose();
